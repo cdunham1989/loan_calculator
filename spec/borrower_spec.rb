33770560e96b426c6ec2ca96738f1a0e31d29287
Chris <cdunham1989@gmail.com>
2018-02-28 17:47:15 +0000

    second unit test for borrower class added to ensure custom loan amounts can be added

--- spec/borrower_spec.rb
+++ spec/borrower_spec.rb
@@ -2,11 +2,14 @@ require 'borrower'
 require 'account'
 
 describe Borrower do
-    before(:each) do
-        @borrower = Borrower.new
-    end
 
     it 'is instantiated with an account with a balance of 10000' do
+        @borrower = Borrower.new
         expect(@borrower.account.loan_balance).to eq(10000)
+    end
+
+    it 'can be instantiated with an account balance of any figure' do
+        @borrower = Borrower.new(Account.new(4000))
+        expect(@borrower.account.loan_balance).to eq(4000)
     end
 end
